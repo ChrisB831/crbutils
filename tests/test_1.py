654a7e5cec4538f899ca0e6e02366c7bb35b5d71
cbon831 <R3GsNETkMrGJrfkzrpwd>
2023-02-28 10:51:07 +0000

    Project restructure

--- tests/test_1.py
+++ tests/test_1.py
@@ -7,14 +7,19 @@ DATE:   28th February 2023
 This test script is called automatically when the code is pushed to the
 remote repo https://github.com/ChrisB831/crbutils
 
-To run in isolation
+To run all test scripts
 1) Go to the project root
-2) python -m pytest .\tests\test1.py -vv
+2) python -m pytest -vv
+
+To run this script in isolation
+1) Go to the project root
+2) python -m pytest .\tests\test_1.py -vv
 '''
 
 
 def test_always_pass():
     '''Dummy test that always passes
+    Useful for testing the CI functionality
 
     Inputs:
         none
@@ -26,6 +31,7 @@ def test_always_pass():
 
 # def test_always_fail():
 #     '''Dummy test that always fails
+#     Useful for testing the CI functionality
 #
 #     Inputs:
 #         none
